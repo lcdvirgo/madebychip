39a6155c452fa62681e268d79d5f930144c4c318
Chip Dong Lim <chip.lim@tradegecko.com>
2020-04-03 23:28:22 +0800

    Create name generator Baby AJs

---
+++ babyaj/script.js
@@ -0,0 +1,192 @@
+$(document).ready(function(){
+		var quoteSource=[
+		{
+			name: "Ava Josephine",
+			meaning: "Ava possesses the vintage Hollywood glam of screen siren Ava Gardner, plus the sleek appeal and allure of compact, modern-sounding names that speed along the popularity lists (like Mia). Ava is one of those elemental names at home in many countries, like Iran (it means “voice” in Persian). In Hebrew it’s seen as related to Chava, which means “life” or “serpent,” or could be descended from the Latin Avis, meaning “bird.” High-profile parents who’ve picked Ava include Reese Witherspoon, Hugh Jackman, Eli Manning, Mia Hamm and Jeremy Renner.",
+			gender:"👧🏻"
+	    },
+	    {
+				name: "Avery Juliet",
+				meaning: "A medieval name, Avery may have magical roots connecting it to the Old English word for elf. Something of a changeling in the modern day, it has gone from primarily a boys' name to a widely popular choice for girls, shooting to the top 50 for American girls in 2007. Fans of the TV comedy 30 Rock will recall Avery Jessup, a no-nonsense reporter played by Elizabeth Banks, while music fans will remember musician Avery Barkley breaking women’s hearts on Nashville. Avery has also made many appearances in children’s fiction, with the male version popping up in Charlotte’s Web and Harry Potter.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Anthony Jacob",
+				meaning: "Anthony derives from Antonius, an old Roman family name that boasts Marc Antony as a member. There are many achievers named Anthony, plus no fewer than half a dozen saints who bear the name, including Saint Anthony of Padua, the patron saint of Italy, and Saint Anthony the Great. Welsh actor Anthony Hopkins is considered to be one of the greatest living actors. Anthonys even work hard when they’re part of the mob, proved by Tony Soprano as the patriarch and protagonist of HBO’s fictional series The Sopranos.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Asher June",
+				meaning: "The Puritans were among the first to dust off this biblical name and introduce it to the English-speaking world. The happy meaning, the soft falling sound of Ash at the beginning and the trendy –er at the finish have helped propel the gentlemanly Asher to become a more approachable choice. In the Bible, Asher was the eighth son of Jacob and promised a life of blessings and abundance. Today, Asher is more popular than ever, propelling its way to the top 100 and beyond.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Adrian Jacob",
+				meaning: "Hadria was a town in northern Italy that gave its name to the Adriatic Sea. The name Adrian means `a man from Hadria`. The name Adrian was a saint's name taken by several early popes, including the only English pope, Adrian IV, and the only Dutch pope, Adrian VI. A popular masculine name in the 20th and 21st centuries, it has an androgynous quality that may make it sound too similar to the female Adrienne for some parents’ tastes. Actors Adrian Grenier and Adrien Brody show two ways to spell the name.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Amelia Juniper",
+				meaning: "From pioneering aviator Amelia Earhart to Amelia Bloomer, a 19th-century women’s rights advocate, Amelia is a name that brings distinction to the bearer, unless you happen to be Minnie Driver. It’s thought to be a combination of Emilia and Amalia or the invention of novelist Henry Fielding. This charming Victorian name is seen as an alternative to the hugely popular Emily. It is also a royal name, as Kings George II and III both named their daughters Amelia. Generations of children have enjoyed the hapless adventures of children’s book character Amelia Bedelia.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Alexander Jackson",
+				meaning: "The defender of mankind has been in popular demand—and in the top 12—since 2005, according to Social Security Administration data. Alexander the Great comes to mind first, but if a world conqueror isn’t enough to persuade you, you might be inspired by American statesman Alexander Hamilton, the subject of a Tony Award–winning Lin-Manuel Miranda musical. You can find Alexanders in the Bible and saints named Alexander as well. Namesakes include actors Alexander Skarsgård and Alexander Dreymon.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Aiden Joseph",
+				meaning: "Aiden is the anglicized version of Aodhan, whose fiery meaning comes from Aodh, the Celtic god of sun and fire. This particular spelling of the Irish name Aidan received a big boost from the arrival of Aiden on Sex and the City, as Mr. Big's rival for Carrie’s affections. According to Social Security data, Aiden hit the top 20 in 2008, riding a wave of similar-sounding names such as Brayden, Caden, Jaden and Kaden. Variants Ayden and Aden are not as popular.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Abigail Jon",
+				meaning: "Abigail was King David's wife in the Bible, and as a girls' name, it has come to represent wisdom. Two Abigails who made their mark on American history were Abigail Adams, the intelligent and resourceful wife of John Adams, and Abigail Williams, one of the primary accusers in the Salem witch trials. (She can also be found in Arthur Miller’s play The Crucible.) Former child actress and working actor Abigail Breslin helped popularize the name in the 2000s. The name is especially popular for fictional characters in both drama and children’s fiction.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Anthony John",
+				meaning: "Anthony derives from Antonius, an old Roman family name that boasts Marc Antony as a member. There are many achievers named Anthony, plus no fewer than half a dozen saints who bear the name, including Saint Anthony of Padua, the patron saint of Italy, and Saint Anthony the Great. Welsh actor Anthony Hopkins is considered to be one of the greatest living actors. Anthonys even work hard when they’re part of the mob, proved by Tony Soprano as the patriarch and protagonist of HBO’s fictional series The Sopranos.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Aria Juno",
+				meaning: "In Hebrew this name means “lioness,” but it is also a musical term derived from an 18th-century Italian word meaning `air` or `tune.` An aria is an expressive melody sung by a single voice, usually in an opera. Either way, Aria hits all the right notes with parents. The TV exposure hasn’t hurt either, as viewers have watched the quirky and beautiful Aria Montgomery on Pretty Little Liars and the fearless and avenging Aryah Stark on Game of Thrones.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Andrew Jack",
+				meaning: "One of Jesus’s 12 Apostles in the Bible, Andrew is the namesake of at least half a dozen saints. With both holy and royal origins, including multiple kings, princes, dukes and US Presidents, Andrew’s reign and influence make this a dominant boys' name in the English-speaking world. Andrew also comes with two clear nicknames, Andy and Drew, and kids will be grateful for the choice as they grow up. British-American actor Andrew Garfield, composer Andrew Lloyd Webber and comedian Andy Samberg are just three of the world’s many famous Andrews.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Aaron Jaxon",
+				meaning: "Aaron has a steadfast and enduring quality. In the Bible he is the brother of Moses and Miriam and the Israelites' first high priest. Variant spellings include Aaryn, Aron (Elvis Presley’s middle name), Arun and Arran. The name Aaron can also be found in literary works by Shakespeare, George Eliot and James Fenimore Cooper. American statesman Aaron Burr famously killed Alexander Hamilton in a duel. West Wing creator Aaron Sorkin and NFL star quarterback Aaron Rodgers are among the modern-day achievers named Aaron.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Angel Justine",
+				meaning: "Angel is a word name that is well known in both the US and Spain. The Greek meaning of the word angel is `messenger,`` but in the Bible, the word signifies a more divine meaning, `messenger from God.`` According to Social Security data, Angel reached the height of its popularity as a boys' name from 2005 to '07, thanks to Spanish-speaking families in the US. It also received a huge boost from Angel, the Buffy the Vampire Slayer spin-off starring David Boreanaz. Today there are more Angels in America than ever before, and some of them are girls.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Aubrey Joella",
+				meaning: "The name Aubrey has much in common with its look-alike pal, Audrey. Both names historically started out as boys’ names but are now used mostly for girls. And both Aubrey and Audrey are lovely and vintage and end on a lively –ee sound. Rap star Drake uses his middle name instead of his first name, Aubrey. Although the name Aubrey dates to the Middle Ages, it has current namesakes in comedic actress Aubrey Plaza of Parks & Recreation and singer-turned-reality TV star Aubrey O’Day,",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Addison Joshua",
+				meaning: "Once considered a boys' name, Addison became hugely popular as a girls' name thanks to TV character Dr. Addison Montgomery, a female doctor on Grey’s Anatomy and Private Practice. The popularity of Madison helped pave the way for Addison’s acceptance as a girls' name, and now the variations Addyson and Addisyn are trending as well. Actor Matthew Lilliard, football player Don Muhlback and baseball players Jason Bay and Mark Teixeira all have daughters named Addison.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Aurora Journey",
+				meaning: "One of the world’s most famous nappers is Aurora, the young princess in “Sleeping Beauty.” At the girl’s birth, the jealous fairy Maleficent put a curse on her to die on her 16th birthday. To thwart that dire plan, Aurora’s protective aunts (the fairies Flora, Fauna and Merryweather) hide her in the forest, where she has a happy life. But shortly before the girl’s Sweet 16, Maleficent discovers where she is, leading Merryweather to put Aurora in a deep sleep to be broken only by a kiss from Prince Phillip, her one true love.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Austin Josiah",
+				meaning: "Austin is derived from the Roman name Augustine, but Americans more fondly know it as the capital of Texas. Several successful athletes carry the name, including NBA player Austin Rivers, wrestler Austin Aries and NFL player Austin Davis. In the past two decades, the name has come to symbolize boyish charm, with pop singer Austin Mahone and fictional spy and ladies man Austin Powers increasing the name's allure.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Adam Jonathan",
+				meaning: "In the Old Testament, Adam is the first man created by God out of clay. The name draws meaning from `adama,` the Hebrew word for “earth.” On the TV show `Girls,`` viewers got a double dose of Adam: Hannah’s on-and-off boyfriend Adam Sackler was played by Adam Driver. And bringing even more fame to the name is Maroon 5 singer Adam Levine.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Audrey Jolene",
+				meaning: "Back in the day, Audrey was the name of beloved sixth-century saint Etheldreda. Shakespeare had a character named Audrey in `As You Like It,` but the biggest influence on the name’s popularity came from starry-eyed fans of the 1961 film `Breakfast at Tiffany’s` and the movie’s glamorous and sophisticated star, Audrey Hepburn. According to Social Security data, Audrey has waltzed in and out of the top 50 girls' names since 2007. Greg Kinnear has a daughter named Audrey, as do Faith Hill and Tim McGraw. Pastry chef and reality TV star Tania Peterson named her daughter Zuzu Audrey in 2011.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Anna Jane",
+				meaning: "Anna has been universally embraced by parents around the world. Russia, Sweden, Norway and Italy are just a few of the countries where Anna is beloved. It’s more distinctive than Ann or Anne and more formal than the nickname Annie. It also blends and pairs easily with other names. Without the letter H, Anna looks like her more popular sister Hannah. Anna has been the name of empresses and queens, as well as actresses such as Pitch Perfect star Anna Kendrick and True Blood actress Anna Paquin. To be even more different, some parents are opting for the Russian nickname Anya.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Axel Joan",
+				meaning: "Axel as a boy's name is pronounced AX-el. It is of Hebrew origin, and the meaning of Axel is `Father is peace`.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Ariana Jericho",
+				meaning: "Pop singer Ariana Grande put this European name on the map, with a little assist from Arianna Huffington, founder of the Huffington Post. Whether you prefer one N or two, Ariana is elegant and romantic, fit for a princess, an opera or running a media company. It might be an Italian form of Ariadne or a Welsh name meaning “silver.” According to Social Security data, Ariana is the preferred spelling and made the top 100 in 2001. It comes with the cute nicknames Ari, Ana and Anna. Tony Dovolani of `Dancing With the Stars` has a daughter named Ariana, as does reality TV star Kim Zolciak Biermann.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Ayden Jeremiah",
+				meaning: "Ayden as a boy's name is of Gaelic origin meaning `fire`.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Allison Jeannine",
+				meaning: "This Scottish diminutive of Alice has been used since the Middle Ages. A top 100 name since 1990s, Allison is now more popular than either Alison or Alice. Once a boys' name (Beverly and Lindsay were too), Allison is now used exclusively for girls. Nicknames include Allie, Alli, Ally, Ali and Al. In `The Canterbury Tales,` the Wife of Bath was called Alisoun, although today the variations are more likely to feature a Y like in Alyson and Allyson. If you’re looking for an even more adventurous and popular spin on this name, try Addison.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Alice Jessamine",
+				meaning: "Alice as a girl's name is pronounced AL-iss. It is of Old German origin, and the meaning of Alice is `noble, exalted`.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Autumn Jessica",
+				meaning: "Autumn is a colorful girls' name that evokes fleeting winds of change and falling leaves, and as a seasonal baby name, it’s more popular than Summer or Spring. The `Au` at the beginning of the name sounds auspicious, while the M adds a note of somber finality. Jennifer Love Hewitt named her daughter Autumn James, and actress Autumn Reeser delivered impressive performances in `Entourage,` `Last Resort` and `Sully.`",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Adeline Jennifer",
+				meaning: "Drop the “M” from Madeline and you get Adeline. A pretty name with many variations—Adelaide, Adalyn, Adalynn, Adelyn—Adeline came from Germany (“adel” means `noble`) and is considered the diminutive of Adele, a French name. It was a huge favorite in the beginning of the 20th century but lost fans as the decades passed. At the turn of the century, it made an astounding comeback as old-fashioned girl names were in vogue again. Simon Helberg from “The Big Bang Theory” named his daughter Adeline in 2012.",
+				gender:"👧🏻"
+	    },
+			{
+				name: "Ashton Jaxson",
+				meaning: "Ashton as a boy's name (also used as girl's name Ashton), is pronounced ASH-ten. It is of Old English origin, and the meaning of Ashton is `ash tree town`.",
+				gender:"👦🏻"
+	    },
+			{
+				name: "Alexa Jasmine",
+				meaning: "Alexa sounds like a name from the future but has deep roots dating to ancient times, when Alexandria, Egypt, was the site of a large library. Techies love the name too, and you may even already have an Alexa in your home in the form of the Amazon Echo. In terms of popularity, Alexa is ahead of Alexandra, Alexis, Alexandria and Alejandra.",
+				gender:"👧🏻"
+	    }
+			// {
+			// 	name: "",
+			// 	meaning: "",
+			// 	gender:"👦🏻👧🏻"
+	    // },
+	];
+
+
+		$('#quoteButton').click(function(evt){
+			//define the containers of the info we target
+			var quote = $('#quoteContainer p').text();
+			var quoteGenius = $('#quoteGenius').text();
+			//prevent browser's default action
+			evt.preventDefault();
+			//getting a new random number to attach to a quote and setting a limit
+			var sourceLength = quoteSource.length;
+			var randomNumber= Math.floor(Math.random()*sourceLength);
+			//set a new quote
+			for(i=0;i<=sourceLength;i+=1){
+			var newBabyName = quoteSource[randomNumber].name;
+			var newBabyMeaning = quoteSource[randomNumber].meaning;
+			var newBabyGender = quoteSource[randomNumber].gender;
+			//console.log(newQuoteText,newQuoteGenius);
+      var timeAnimation = 300;
+      var quoteContainer = $('#quoteContainer');
+      //fade out animation with callback
+      quoteContainer.fadeOut(timeAnimation, function(){
+        quoteContainer.html('');
+				quoteContainer.append('<h1>'+newBabyName+'</h1>'+'<h1 style="margin:0;">'+newBabyGender+'</h1>'+'<p>'+newBabyMeaning+'</p>');
+        //fadein animation.
+        quoteContainer.fadeIn(timeAnimation);
+      });
+
+			break;
+		};//end for loop
+
+	});//end quoteButton function
+
+
+});//end document ready
